2fb02ea0c39fb9ae039eac7851ce74a3a5da2dee
John Haver Herrera <johnhaverh@gmail.com>
2023-09-21 17:48:32 -0500

    add role validation from DB

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
+const Role = require('../models/role');
 
 const { usuariosGet, 
         usuariosPut, 
@@ -18,7 +19,14 @@ router.post('/', [
   check('nombre','Nombre es obligatorio').not().isEmpty(),
   check('correo','Formato correo errado').isEmail(),
   check('password','Password es tener minimo 6 caracteres').isLength({min: 6}),
-  check('rol','Rol no es valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+  // check('rol','Rol no es valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+  check('rol').custom( async (rol = '') =>{
+    const existeRol = await Role.findOne({rol});
+    if (!existeRol){
+      throw new Error(`Rol: ${ rol} no existe en DB`)
+    }
+
+  }),
   validarCampos
 ], usuariosPost )
 router.patch('/', usuariosPatch )
