9e34308d53b18c3d0dde0374ffde230c62cf5edf
John Haver Herrera <johnhaverh@gmail.com>
2023-09-22 15:27:15 -0500

    add email & password validation for login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,44 @@
 const {response} = require('express');
+const bcryptjs = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
 
 const login = async (req = request, res = response) => {
 
-    res.json({
-      msg: "Login ok",
-    })
+    const { correo, password} = req.body;
+
+    try {
+      const usuario = await Usuario.findOne( {correo} );
+      
+      if (!usuario){
+        return res.status(400).json({
+          msg: 'Usuario / Password no validos - correo errado',
+        })
+      }
+
+      if (!usuario.estado){
+        return res.status(400).json({
+          msg: 'Usuario / Password no validos - inactivo',
+        })
+      }
+
+      const validaPassword = bcryptjs.compareSync(password, usuario.password)
+
+      if (!validaPassword){
+        return res.status(400).json({
+          msg: 'Usuario / Password no validos - password errado',
+        })
+      }
+
+      res.json({
+        msg: "Login ok",
+      })
+    } catch (error) {
+      return res.status(500).json({
+        msg: 'Fallo el login - contactar al administrador',
+      })
+    }
+
   }
 
 
