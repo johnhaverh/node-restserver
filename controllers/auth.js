5d89b9233e7eaef04cbb571c4facad3b41b65292
John Haver Herrera <johnhaverh@gmail.com>
2023-09-26 16:25:24 -0500

    create user using information gy google auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req = request, res = response) => {
 
@@ -50,12 +51,46 @@ const login = async (req = request, res = response) => {
 const googleSignIn = async (req, res=response) => {
     const  {id_token} = req.body;
 
-    res.json({
-      msg: 'Conexión Google Ok',
-      id_token
-    })
+    try {
+      const {nombre, img, correo} = await googleVerify(id_token);
+      
+      let usuario = await Usuario.findOne( {correo} );
 
-}
+      if (!usuario){
+          const data = {
+              nombre,
+              correo,
+              password: ':P',
+              img,
+              google: true,
+          };
+
+          usuario = new Usuario (data);
+          await usuario.save();
+      }
+
+      if (!usuario.estado) {
+        return res.status(401).json({
+          msg: `Usuario bloqueado - validar con administrador`,
+        })
+      }
+
+      //generacion JWT
+      const token = await generarJWT(usuario.id);
+
+      res.json({
+        usuario,
+        token,
+        msg: `Validacion OK - Google`,
+      })
+    } catch (error) {
+      return res.status(400).json({
+        msg: `Fallo Google login - contactar al administrador ${error}`,
+      })
+    }
+    
+
+  }
 
 module.exports = {
     login,
